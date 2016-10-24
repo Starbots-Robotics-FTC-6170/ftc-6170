208c6b5139b0f0157a18a4c1f16b0d78871c2241
Foster McLane <fkmclane@gmail.com>
2016-10-24 12:44:45 -0400

    add basic structure for robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoNameAuto.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name="NoName Auto", group="NoName")
+public class NoNameAuto extends OpMode {
+    NoName bot = new NoName();
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        bot.init(hardwareMap);
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
