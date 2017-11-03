aa0b0114ee8b6d668930332f9d00051ae2f4487b
Foster McLane <fkmclane@gmail.com>
2017-11-03 09:58:10 -0400

    fix indentatino in SupernovaCornerOnly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SupernovaCornerOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SupernovaCornerOnly.java
@@ -1,34 +1,31 @@
-    package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
 
-    import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-    import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-    import com.qualcomm.robotcore.hardware.DcMotor;
-    import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-    @Autonomous(name = "SupernovaCornerOnly", group = "Supernova")
-    public class SupernovaCornerOnly extends LinearOpMode {
-        private double AWAY_FROM_WALL = 0.8;
-        private double TURN_CORNER = 0.40;
-        private double PARK_VORTEX = 0.80;
+@Autonomous(name = "SupernovaCornerOnly", group = "Supernova")
+public class SupernovaCornerOnly extends LinearOpMode {
+    private final double AWAY_FROM_WALL = 0.8;
+    private final double TURN_CORNER = 0.40;
+    private final double PARK_VORTEX = 0.80;
 
-        private Supernova bot = new Supernova();
+    private Supernova bot = new Supernova();
 
-        //*Code to run ONCE when the driver hits INIT
+    //*Code to run ONCE when the driver hits INIT
 
-        @Override
-        public void runOpMode() {
-            bot.init(hardwareMap, telemetry);
+    @Override
+    public void runOpMode() {
+        bot.init(hardwareMap, telemetry); // Initialize the robot
 
-            waitForStart();
+        waitForStart(); // Waits between initialization and match start
 
-            bot.move(-AWAY_FROM_WALL, -AWAY_FROM_WALL);
-            bot.move(TURN_CORNER, -TURN_CORNER);
-            bot.move(-PARK_VORTEX, -PARK_VORTEX);
-            bot.sweepAuto(true);
-            sleep(2000);
-            bot.sweepAuto(false);
-
-
-        }
-
+        bot.move(-AWAY_FROM_WALL, -AWAY_FROM_WALL); // go backward
+        bot.move(TURN_CORNER, -TURN_CORNER); // turn right
+        bot.move(-PARK_VORTEX, -PARK_VORTEX); // go backward
+        bot.sweepAuto(true); // turn on sweep
+        sleep(2000); // wait 2 seconds
+        bot.sweepAuto(false); // turn off sweep
     }
+}
