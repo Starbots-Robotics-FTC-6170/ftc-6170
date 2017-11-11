ba25ccea21173e87cf9f1f0332a996292840293f
Foster McLane <fkmclane@gmail.com>
2017-11-11 11:49:36 -0500

    add start of SupernovaBalancingStone

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SupernovaBalancingStone.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Supernova Autonomous Balancing", group = "Autonomous")
+public class SupernovaBalancingStone extends LinearOpMode {
+    private Supernova bot = new Supernova();
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void runOpMode() {
+        bot.init(hardwareMap, telemetry);
+
+        waitForStart();
+    }
+}
